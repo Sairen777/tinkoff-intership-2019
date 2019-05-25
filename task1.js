85b9b948dcc5b65fdedbf9f231e096c1adf580bc
Alexander Kucherenko <aksairensan@gmail.com>
2019-05-25 14:20:53 +0500

    first task done

---
+++ task1.js
@@ -0,0 +1,12 @@
+function justAddOne(array, number) {
+  let strFromArray = '';
+  for (const item of array) {
+    if (Number.isInteger(item) && item >= 0 && item <= 9) {
+      strFromArray += item.toString();
+    } else {
+      return null;
+    }
+  };
+  const strResult = (parseInt(strFromArray) + parseInt(number)).toString();
+  return [...strResult].map(digit => parseInt(digit));
+}
